2c5c3914533ff2029dceea047ae1af4e0f232a55
kerojam <majorekcs@hotmail.com>
2023-12-27 20:47:37 -0600

    fixed battery kanji

--- blocks.h
+++ blocks.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 {"| メモリ:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
-    {"バッテリー:", "acpi | awk '{print $4}' | sed s/,//",          5,       0},
+    {"電池:", "acpi | awk '{print $4}' | sed s/,//",          5,       0},
 	{"年", "date '+%Y/月%m/日%d | 時間%H:%M '",                  5,       0},
 };
 
